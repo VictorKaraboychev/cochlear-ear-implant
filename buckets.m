681ddcf064cf1c67cc9db5ea362e2a3994d4ca59
sdellen <91271028+sdellen@users.noreply.github.com>
2023-11-16 11:23:10 -0500

    added to buckets.m

--- buckets.m
+++ buckets.m
@@ -1,37 +1,53 @@
 % Example usage with the Greenwood function
-min_frequency = 100; % Minimum frequency in Hz
-max_frequency = 12800; % Maximum frequency in Hz
-numBins = 7; % Number of bins
+f_min = 100; % Minimum frequency in Hz
+f_max = 12800; % Maximum frequency in Hz
+N = 7; % Number of bins
 
 % Specify the Greenwood function as the tonotopic model
-greenwoodModel = @(x) 165.4 * (10.^(2.1*x - 0.06)) - 0.11;
-linearModel = @(x) min_frequency + x * (max_frequency - min_frequency);
-logarithmicModel = @(x) min_frequency * 10.^(x * log10(max_frequency/min_frequency));
-sqrtModel = @(x) min_frequency + x.^2 * (max_frequency - min_frequency);
-exponentialModel = @(x) min_frequency * exp(x * log(max_frequency/min_frequency));
-tanhModel = @(x) min_frequency + 0.5 * (tanh(2*x - 1) + 1) * (max_frequency - min_frequency);
-doublingBinSizeModel = @(x) min_frequency * 2.^(x * log2(max_frequency/min_frequency));
+greenwoodModel = @(x) (165.4 * (10.^(2.1*x - 0.06)) - 0.11);
+linearModel = @(x) f_min + x * (f_max - f_min);
+logarithmicModel = @(x) f_min * 10.^(x * log10(f_max/f_min));
+% logarithmicModel = @(x) f_min * (f_max/f_min).^x;
+sqrtModel = @(x) f_min + x.^2 * (f_max - f_min);
+exponentialModel = @(x) f_min * exp(x * log(f_max/f_min));
+tanhModel = @(x) f_min + 0.5 * (tanh(2*x - 1) + 1) * (f_max - f_min);
+doublingBinSizeModel = @(x) f_min * 2.^(x * log2(f_max/f_min));
+
+% logarithmicModel, exponentialModel, and doublingBinSizeModel are identical
+% tanh and greenwood dont seem right...
+
+% figure;
+bins = calc_bins(f_min, f_max, N, greenwoodModel);
+bins = calc_bins(f_min, f_max, N, linearModel);
+bins = calc_bins(f_min, f_max, N, logarithmicModel);
+bins = calc_bins(f_min, f_max, N, exponentialModel);
+bins = calc_bins(f_min, f_max, N, doublingBinSizeModel);
+bins = calc_bins(f_min, f_max, N, sqrtModel);
+bins = calc_bins(f_min, f_max, N, tanhModel);
 
 
-
-
-% Call the cochlearImplantFilterBank function with the specified tonotopic model
-binSizes = cochlearImplantFilterBank(min_frequency, max_frequency, numBins, doublingBinSizeModel);
-
-function binSizes = cochlearImplantFilterBank(min_frequency, max_frequency, numBins, tonotopicModel)
+function bins = calc_bins(min_frequency, max_frequency, numBins, tonotopicModel)
     % Calculate the normalized distance along the cochlea for each bin
     x = linspace(0, 1, numBins+1);
 
     % Calculate the characteristic frequencies using the specified tonotopic model
-    characteristicFrequencies = tonotopicModel(x);
+    bins = tonotopicModel(x);
 
     % Calculate the bin sizes as the absolute difference between consecutive characteristic frequencies
-    binSizes = abs(diff([min_frequency, characteristicFrequencies, max_frequency]));
+    sizes = abs(diff([min_frequency, bins, max_frequency]));
 
     % Display the characteristic frequencies and bin sizes
     format shortG
     disp('Bins (Hz):');
-    disp(characteristicFrequencies);
+    disp(bins);
     % disp('Bin Sizes (Hz):');
     % disp(binSizes);
+
+    % Plot the characteristic frequencies
+    % nexttile;
+    % plot(x, bins, 'o-', 'LineWidth', 2);
+    % xlabel('Normalized Distance Along Cochlea');
+    % ylabel('Characteristic Frequency (Hz)');
+    % title('Tonotopic Model: Characteristic Frequencies');
+    % grid on;
 end
